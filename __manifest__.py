a1bec3654612494b4ea8c5032b6d120ccc7598ba
Sebastien LANGE <sebastien.lange@syleam.fr>
2017-03-27 20:58:28 +0200

    [IMP] Improve manifest file

--- __manifest__.py
+++ __manifest__.py
@@ -3,11 +3,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'intervention',
+    'name': 'Intervention',
     'version': '10.0.1.0.0',
     'category': 'Custom',
     'summary': 'Managing Intervention Requests',
-    'author': 'Sebastien LANGE',
+    'author': 'Syleam',
     'website': 'http://www.syleam.fr',
     'depends': [
         'base',
